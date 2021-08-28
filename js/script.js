b869e3d4cb4d6b7268f8cf003ba1a4a0cfbf07f8
Eman73 <ma3920457@gmail.com>
2021-08-28 22:55:04 +0200

    first commit

--- js/script.js
+++ js/script.js
@@ -11,5 +11,32 @@ $(document).ready(function(){
         $('.opacity').fadeOut(700);
         $('html').css("overflow-y", "auto");
     });
+
+    window.addEventListener('scroll', $('.counter').counterUp({
+        delay: 30,
+        time: 1500
+    }));
 });
+    ///////
+
+// let counters = document.querySelectorAll('.counter');
+// const speed = 100;
+
+// counters.forEach(counter=>{
+//     const updateCount =() =>{
+//         const target = +counter.getAttribute('data-target'),
+//             count = +counter.innerText,
+//             inc = target/speed;
+    
+//         if(count < target){
+//             counter.innerText = count + inc;
+//             setTimeout(updateCount, 1);
+//         }
+//         else{
+//             count.innerText = target;
+//         }
+//     }
+//     updateCount();
+// });
+
 //end section2
